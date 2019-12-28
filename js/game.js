a5f1a3b917b49288dfa1f9a103d94febc96158d6
Tamir Nakar <nakar.tamir@gmail.com>
2019-12-28 20:31:49 +0200

    init

---
+++ js/game.js
@@ -0,0 +1,15 @@
+const config = {
+  width: 256,
+  height: 272,
+  backgroundColor: 0x000000,
+  scene: [Scene1, Scene2],
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  }
+};
+
+const game = new Phaser.Game(config);
---
+++ js/scenes/Scene1.js
@@ -0,0 +1,36 @@
+class Scene1 extends Phaser.Scene {
+  constructor() {
+    super("bootGame");
+  }
+
+  preload() {
+    this.load.image("bg", "../assets/images/background.png");
+    //this.load.image("ship1", "../assets/images/ship1.png");
+    //this.load.image("ship2", "../assets/images/ship2.png");
+    //this.load.image("ship3", "../assets/images/ship3.png");
+    this.load.spritesheet("ship1", "../assets/spritesheets/ship1.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.spritesheet("ship2", "../assets/spritesheets/ship2.png", {
+      frameWidth: 32,
+      frameHeight: 16
+    });
+    this.load.spritesheet("ship3", "../assets/spritesheets/ship3.png", {
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    this.load.spritesheet("explosion", "../assets/spritesheets/explosion.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+    this.load.spritesheet("power-up", "../assets/spritesheets/power-up.png", {
+      frameWidth: 16,
+      frameHeight: 16
+    });
+  }
+  create() {
+    this.add.text(20, 20, "Loading Game...");
+    this.scene.start("playGame");
+  }
+}
